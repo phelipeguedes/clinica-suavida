b8b0bd39c9f70ab32692fe21006eb63aef34d50e
phelipeguedes <phelipe.guedes@hotmail.com>
2016-05-18 13:55:45 -0300

    projeto em andamento

--- cadastro-medico/atualizar-medico_form.php
+++ cadastro-medico/atualizar-medico_form.php
@@ -44,39 +44,23 @@
                 <label class="lblcadastro">Identificação:<input type="text"
 					name="id" id="id" class="txtcadastro" placeholder="id"
 					required /></label><br /> 
-				<label class="lblcadastro">Nome:<input type="text"
-					name="nome" id="nome" class="txtcadastro" placeholder="nome"
-					required /></label><br /> <label class="lblcadastro">CRM:<input
-					type="text" name="crm" id="crm" class="txtcadastro"
-					placeholder="crm" required /></label><br /> <label class="lblcadastro">UF:<input
-					type="text" name="uf" id="uf" class="txtcadastro" placeholder="uf"
-					required /></label><br /> <label class="lblcadastro">Nascimento:<input
-					type="date" name="nascimento" id="nascimento" class="txtcadastro"
-					placeholder="data de nascimento" required /></label><br /> <label
-					class="lblcadastro">Especialidade:<input type="text"
-					name="especialidade" id="especialidade" class="txtcadastro"
-					placeholder="especialidade" required /></label><br /> <label
-					class="lblcadastro">Email:<input type="email" name="email"
-					id="email" class="txtcadastro" placeholder="email" required /></label><br />
-				<br /> Sexo:<label class="sexo">Masculino<input type="radio"
-					name="sexo" id="masculino" value="masculino" required /></label> <label
-					class="sexo" id="fem">Feminino<input type="radio"
-					name="sexo" id="feminino" value="feminino" required /></label><br />
-				<br /> Fazendo residência?:<label class="residencia" id="sim">Sim<input
-					type="radio" name="residencia" id="sim" value="sim" required /></label> <label>Não<input
-					type="radio" name="residencia" id="nao" value="nao" required /></label><br/><br/>
+				<label class="lblcadastro">Nome:<input type="text"	name="nome" id="nome" class="txtcadastro" placeholder="nome" required/></label><br/>
+				<label class="lblcadastro">CRM:<input type="text" name="crm" id="crm" class="txtcadastro" placeholder="crm" required /></label><br/>
+				<label class="lblcadastro">UF:<input type="text" name="uf" id="uf" class="txtcadastro" placeholder="uf"	required /></label><br/>
+				<label class="lblcadastro">Nascimento:<input type="date" name="nascimento" id="nascimento" class="txtcadastro" placeholder="data de nascimento" required /></label><br/>
+				<label class="lblcadastro">Especialidade:<input type="text" name="especialidade" id="especialidade" class="txtcadastro"	placeholder="especialidade" required /></label><br/>
+				<label class="lblcadastro">Email:<input type="email" name="email" id="email" class="txtcadastro" placeholder="email" required /></label><br/><br/> 
+
+				Sexo:<label class="sexo">Masculino<input type="radio" name="sexo" id="masculino" value="masculino" required /></label>
+				<label class="sexo" id="fem">Feminino<input type="radio" name="sexo" id="feminino" value="feminino" required /></label><br/><br/>
+				Fazendo residência?:<label class="residencia" id="sim">Sim<input type="radio" name="residencia" id="sim" value="sim" required /></label> 
+				<label>Não<input type="radio" name="residencia" id="nao" value="nao" required /></label><br/><br/>
 
 				<aside id="lado">
-					<label class="lbllado">Login:<input type="text"
-						name="login" id="login" class="txtcadastrolado"
-						placeholder="nome de usuario" required /></label><br />
-					<br />
-					<br /> <label class="lbllado">Senha:<input type="password"
-						name="senha" id="senha" class="txtcadastrolado"
-						placeholder="senha" required /></label><br />
+					<label class="lbllado">Login:<input type="text"	name="login" id="login" class="txtcadastrolado"	placeholder="nome de usuario" required/></label><br/><br/><br/>
+					<label class="lbllado">Senha:<input type="password"	name="senha" id="senha" class="txtcadastrolado" placeholder="senha" required/></label><br/>
 					<p>
-						<input type="submit" value="atualizar" class="lbllado"
-							id="btcadastrar" onclick="validarFormMedico();" />
+						<input type="submit" value="atualizar" class="lbllado" id="btcadastrar" onclick="validarFormMedico();"/>
 					</p>
 				</aside>
 
